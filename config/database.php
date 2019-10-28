b78c56433e0d21658db1a545d2da0089cd64abf5
hcrobert <c.habimana@irembo.com>
2019-10-28 12:11:07 +0200

    lasjfaklfj

--- config/database.php
+++ config/database.php
@@ -5,7 +5,7 @@ class Database{
     private $host = "localhost";
     private $db_name = "alliance";
     private $username = "root";
-    private $password = "";
+    private $password = "admin";
     public $conn;
 
 
